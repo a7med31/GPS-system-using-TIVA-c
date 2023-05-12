cff5a9cb7a3291972462f695bf32e9397ba91f3f
MazenZein1512 <132165303+MazenZein1512@users.noreply.github.com>
2023-05-12 21:53:38 +0300

    Port F initialization

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include "tm4c123gh6pm.h"
 #include "stdint.h"
 #include "stdlib.h"
+void PortF_Init(void);
 
-int main(){}
+int main(){
+void PortF_Init(void)
+{
+    SYSCTL_RCGCGPIO_R |= 0X20;                      //start clock
+    while(!(SYSCTL_PRGPIO_R & 0x20));               //check clock status
+    GPIO_PORTF_DEN_R |= 0x0E;                       //enable digital
+    GPIO_PORTF_AMSEL_R &= ~0x0E;                    //disable analog
+    GPIO_PORTF_AFSEL_R &= ~0x0E;                    //no alternative function
+    GPIO_PORTF_PCTL_R &= ~0xFFF0;                   //disable PCTL
+    GPIO_PORTF_DIR_R |= 0x0E;                       //3 output LEDs
+    GPIO_PORTF_DATA_R &= 0X0E;                      //leds are off initially
+}
+}
