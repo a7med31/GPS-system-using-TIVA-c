8e14becb2895b3aaa3d557f90f6660727b6296c2
a7med31 <ahmed.abdulrahman3000@gmail.com>
2023-05-12 23:47:35 +0300

    main function

--- main.c
+++ main.c
@@ -27,6 +27,7 @@ void printflo(float x);
 void reverse(char* str, int len);
 int intToStr(int x, char str[], int d);
 void ftoa(float n, char* res, int afterpoint);
+float input_latlong(float latlong);
 
 //global variables
 char latitude[100], longitude[100], command[100];
@@ -36,9 +37,107 @@ float lat_deg, long_deg, c_lat, c_long, p_lat, p_long, f_lat, f_long, s_lat, s_l
 int lat_coordinate, long_coordinate;
 int flag, len, first;
 
+float DELTA_ERROR = 0.7;
+int GPS_ERROR = 7;
+
 int main()
 {
-    
+    FPUEnable();
+    UART0_Init();
+    UART2_Init();
+    PortF_Init();
+	 
+    first = 0;
+    total_distance = 0;
+
+
+    //Enter destination coordinates here
+    f_lat = input_latlong(30.0643838);
+    f_long = input_latlong(31.2800895);
+   
+
+		
+    while(1)
+    {
+        memset(command,0,strlen(command));   //Empty the command array.
+        getCommand(command);                 //Read the data from GPS module and store in command array.
+
+        
+        if (first == 0) {       //to store the starting coordinates of the GPS.
+            getCoordinates();
+            if(flag ==1){      //if the gps reading is not valid continue and take another reading.
+                continue;
+            }
+            s_lat = torad(lat_coordinate, lat_deg);   //starting latitude
+            s_long = torad(long_coordinate, long_deg);//starting longtiude
+            c_lat = s_lat;                            //current latitude
+            c_long = s_long;                          //current longtitude
+            
+        }
+        else {
+            p_lat = c_lat;
+            p_long = c_long;
+            getCoordinates();
+            if(flag ==1){
+                continue;
+            }
+             c_lat = torad(lat_coordinate, lat_deg);
+             c_long = torad(long_coordinate, long_deg);
+             delta_view = delta(p_lat, p_long, c_lat, c_long);
+             if (!(delta_view<DELTA_ERROR))
+             {
+                total_distance += delta(p_lat, p_long, c_lat, c_long);   // total distance calculation by adding the distances calculated between each 2 consecutive readings.  
+             }
+             
+        }
+
+
+        //Condition to check the format is right first before getting distane to goal dtg.
+        if (flag != 1) //The format is not wrong
+        {
+            dtg = delta(c_lat, c_long, f_lat, f_long) - GPS_ERROR ;
+        }
+            
+        
+		if (dtg <= 0) { // if the destination is reached turn the green led on.
+            GPIO_PORTF_DATA_R = 0x08;
+			printStr("\n\n");
+            printStr("Latitude: ");
+            printflo(lat_float);
+            printStr("\tLongitude: ");
+            printflo(long_float);
+            printStr("\nTotal distance traveled: ");
+            printflo(total_distance);
+            printStr("\nYou've reached your distination :)\n");
+            while(1);
+        }
+        else if (dtg < 5) { // if only 5m is left to reach the destination turn the yellow led on.
+            GPIO_PORTF_DATA_R = 0x0A;
+            printStr("Latitude: ");
+            printflo(lat_float);
+            printStr("\tLongitude: ");
+            printflo(long_float);
+            printStr("\nTotal distance traveled: ");
+            printflo(total_distance);
+            printStr("\nYou're close, less than 5m left\n");
+            printStr("\n\n");
+
+        }
+        else if (dtg > 5) { // if more than 5m is left to reach the destination turn the red led on.
+            GPIO_PORTF_DATA_R = 0x02;
+            printStr("Latitude: ");
+            printflo(lat_float);
+            printStr("\tLongitude: ");
+            printflo(long_float);
+            UART0_write('\n');
+            printStr("Total distance traveled:");
+            printflo(total_distance);
+						printStr("\n\n");
+            UART0_write('\n');
+
+
+        }
+}
 }
 
 void PortF_Init(void)
@@ -135,7 +234,7 @@ void getCommand(char*str)
         }
         else
             str[i] = c; 
-    }
+    }
 }
 void parse(void)
 {
@@ -163,7 +262,7 @@ for (i = 0; i < 5; i++){
         flag = 1;
         return;
     }
-    first = 1; //Moved the first flag into the parse funtion to always trigger after finding the right format the first time
+    first = 1; 
     j = 0;
     for (i = 18; i < 28; i++)
     {
@@ -265,5 +364,6 @@ void ftoa(float n, char* res, int afterpoint) //Check number of floating digits
     }
 }
 float input_latlong(float latlong){
+	
     return torad((int)latlong, floor(100000*((latlong - (int)latlong) * 60))/100000);
 }
---
+++ main.d
@@ -0,0 +1,7 @@
+.\main.o: main.c
+.\main.o: tm4c123gh6pm.h
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
+.\main.o: fpu.h
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\math.h
