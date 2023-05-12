6efae79a28ad51054bc4a737cb2cd4615fd10192
Mina020 <131830799+Mina020@users.noreply.github.com>
2023-05-12 22:02:08 +0300

    fpu initialization , print string and UART0 write

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include "tm4c123gh6pm.h"
 #include "stdint.h"
 #include "stdlib.h"
+#include "fpu.h"
 
+#define NVIC_CPAC               0xE000ED88  // Coprocessor Access Control
+#define HWREG(x)                (*((volatile uint32_t *)(x)))
+void FPUEnable(void);
 void PortF_Init(void);
-
+void UART0_write(char c);
+void printStr(char *str);
 int main()
 {
     
@@ -21,3 +26,28 @@ void PortF_Init(void)
     GPIO_PORTF_DATA_R &= 0X0E;                      //leds are off initially
 }
 
+void FPUEnable(void)
+{
+    //
+    // Enable the coprocessors used by the floating-point unit.
+    //
+    HWREG(NVIC_CPAC) = ((HWREG(NVIC_CPAC) &
+                         ~(NVIC_CPAC_CP10_M | NVIC_CPAC_CP11_M)) |
+                        NVIC_CPAC_CP10_FULL | NVIC_CPAC_CP11_FULL);
+}
+void UART0_write(char c)
+{
+    while ((UART0_FR_R & UART_FR_TXFF) != 0);
+    UART0_DR_R = c;
+}
+
+void printStr(char *str)
+{
+    uint8_t i = 0;
+    while(str[i])
+    {
+        char c = str[i];
+        UART0_write(c);
+        i++;
+    }
+}
