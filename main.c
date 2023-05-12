40a96204eff161bae05e5b5eff4cf09b8f8c985a
bibo5690 <87624343+bibo5690@users.noreply.github.com>
2023-05-12 22:08:50 +0300

    Parsing function

--- main.c
+++ main.c
@@ -2,13 +2,23 @@
 #include "stdint.h"
 #include "stdlib.h"
 #include "fpu.h"
+#include "string.h"
 
+//FPU 
 #define NVIC_CPAC               0xE000ED88  // Coprocessor Access Control
 #define HWREG(x)                (*((volatile uint32_t *)(x)))
+
+//functions definitions
 void FPUEnable(void);
 void PortF_Init(void);
 void UART0_write(char c);
 void printStr(char *str);
+void parse(void);
+char* substring(char *destination, const char *source, int beg, int n);
+
+//global variables
+char latitude[100], longitude[100], command[100];
+
 int main()
 {
     
@@ -50,4 +60,64 @@ void printStr(char *str)
         UART0_write(c);
         i++;
     }
+}
+
+void parse(void)
+{
+    char check[100];
+    int i, j; 
+    flag = 0;
+
+ //CHECK
+for (i = 0; i < 5; i++){
+        check[i] = command[i];
+}
+    check[5] = '\0';
+    if ((strcmp(check, "GPRMC")) != 0)
+    {
+        flag = 1;
+        // printStr("Error1");
+        return;
+    }
+    if (command[16] != 'A')
+		{
+        // printStr("Error2");
+        flag = 1;
+
+        return;
+    }
+    if (command[17] != ',')
+		{
+        // printStr("Error3");
+        flag = 1;
+
+        return;
+    }
+    first = 1; //Moved the first flag into the parse funtion to always trigger after finding the right format the first time
+    j = 0;
+    for (i = 18; i < 28; i++)
+    {
+        latitude[j] = command[i]; //un seperated lat
+        j += 1;
+    }
+    j = 0;
+    for (i = 31; i < 42; i++)
+    {
+        longitude[j] = command[i];
+        j += 1;
+    }
+}
+
+char* substring(char *destination, const char *source, int beg, int n)
+{
+    while (n > 0)
+    {
+        *destination = *(source + beg);
+ 
+        destination++;
+        source++;
+        n--;
+    }
+    *destination = '\0';
+    return destination;
 }
