bf0379cf42153b233ae0804d2b511069dc9f4236
Mohammed Yasser <mohdyasser100@gmail.com>
2023-05-12 22:17:45 +0300

    UART Initialization
    UART0 and UART2 Initialization
    By: Mohammed Yasser 2002085

--- main.c
+++ main.c
@@ -6,18 +6,23 @@
 
 //FPU 
 #define NVIC_CPAC               0xE000ED88  // Coprocessor Access Control
-#define HWREG(x)                (*((volatile uint32_t *)(x)))
+#define HWREG(x)                (*((volatile uint32_t *)(x)))
 
 //functions definitions
 void FPUEnable(void);
 void PortF_Init(void);
 void UART0_write(char c);
-void printStr(char *str);
+void printStr(char *str);
 void parse(void);
 char* substring(char *destination, const char *source, int beg, int n);
+void UART0_Init(void);
+void UART2_Init(void);
+char UART2_read(void);
+
 
 //global variables
 char latitude[100], longitude[100], command[100];
+int flag, len, first;
 
 int main()
 {
@@ -45,6 +50,51 @@ void FPUEnable(void)
                          ~(NVIC_CPAC_CP10_M | NVIC_CPAC_CP11_M)) |
                         NVIC_CPAC_CP10_FULL | NVIC_CPAC_CP11_FULL);
 }
+
+void UART0_Init(void)
+{
+    SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R0;                       //Enable UART clock
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0;                       //Enable GPIO clock
+
+    UART0_CTL_R &= ~UART_CTL_UARTEN;                               //Disable UART CTL
+    
+    UART0_IBRD_R = 104;                                            //Calculate BRD: (16*10^6)/(16*9600) = 104.16667
+    UART0_FBRD_R = 11;                                             //int(0.16667*64 + 0.5) = 11
+
+    UART0_LCRH_R = (UART_LCRH_WLEN_8 | UART_LCRH_FEN);             //Enable UART parameters such as data length & FIFO
+    UART0_CTL_R |= (UART_CTL_UARTEN | UART_CTL_RXE | UART_CTL_TXE);//Enable UART CTL
+
+    GPIO_PORTA_AFSEL_R |= 0x03;                                    //Enable UART Alternate Function for PA0 & PA1
+    GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & ~0xFF) | (GPIO_PCTL_PA0_U0RX | GPIO_PCTL_PA1_U0TX);//PA0-->RX & PA1-->TX
+    GPIO_PORTA_DEN_R |= 0x03;                                      //Digital Enable for 2pins        
+}
+
+void UART2_Init(void)
+{
+    SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R2;                       //enable UART clock
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R3;                       //enable GPIO clock
+	while(!(SYSCTL_PRUART_R&SYSCTL_PRUART_R2 ));                   //Wait for the Clock to start
+	
+    UART2_CTL_R &= ~UART_CTL_UARTEN;                               //disable UART CTL
+    
+    UART2_IBRD_R = 104;                                            //calculate BRD: (16*10^6)/(16*9600) = 104.16667
+    UART2_FBRD_R = 11;      //0.16667*64 + 0.5
+
+    UART2_LCRH_R = 0x70;  //enable uart parameters such as data length & FIFO
+    UART2_CTL_R |= (UART_CTL_UARTEN | UART_CTL_RXE | UART_CTL_TXE);             //enable UART CTL
+
+    GPIO_PORTD_LOCK_R = GPIO_LOCK_KEY;
+    GPIO_PORTD_CR_R |= 0x080;
+    GPIO_PORTD_AFSEL_R |= 0x0C0;
+    GPIO_PORTD_PCTL_R |= (GPIO_PCTL_PD6_U2RX | GPIO_PCTL_PD7_U2TX);
+    GPIO_PORTD_DEN_R |= 0x0C0;
+}
+
+char UART2_read(void)
+{    while ((UART2_FR_R & UART_FR_RXFE) != 0);
+    return UART2_DR_R & 0xFF;
+}
+
 void UART0_write(char c)
 {
     while ((UART0_FR_R & UART_FR_TXFF) != 0);
@@ -58,8 +108,8 @@ void printStr(char *str)
     {
         char c = str[i];
         UART0_write(c);
-        i++;
-    }
+        i++;
+    }
 }
 
 void parse(void)
